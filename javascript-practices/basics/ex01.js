9adfe7843c43f6ba4c5a8bd64ecc8514bee7d27d
wizdom972 <jihae0584@gmail.com>
2025-02-03 16:58:12 +0900

    📝 2025-02-03 수업 내용
    - JavaScript
    - 01 변수와 자료형
    - 02 표현식과 구문문

--- javascript-practices/basics/ex01.js
+++ javascript-practices/basics/ex01.js
@@ -1,4 +1,34 @@
 console.log("============[01] 기본타입, primitive type");
+
 console.log("============[02.01] 객체타입: object");
+var i = new Number(10);
+var s = new String("c");
+var b = new Boolean(true);
+var o = {};
+var a = [];
+var n = null;
+
+console.log("i: ", typeof i);
+console.log("s: ", typeof s);
+console.log("b: ", typeof b);
+console.log("o: ", typeof o);
+console.log("a: ", typeof a);
+console.log("n: ", typeof n);
+
 console.log("============[02.02] 객체타입: function");
+function f1() {}
+var f2 = function () {};
+var f3 = new Function();
+
+console.log("f1: ", typeof f1);
+console.log("f2: ", typeof f2);
+console.log("f3: ", typeof f3);
+
 console.log("============[06] 기본타입과 유사객체");
+var b1 = true;
+var b2 = new Boolean(false);
+
+console.log(b1.valueOf()); // console.log(new Boolean(b1).valueOf());
+console.log(b2.valueOf());
+
+console.log(typeof typeof null);
--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -1,3 +1,11 @@
 console.log("============[02.02] 표현식(Expression)");
+i = 4;
+2 + 3;
+parseInt("5");
+4;
+i;
+
+var j;
+
 console.log("============[05] 세미콜론(Semicolon)");
 console.log("============[06] 개행(Line Break)");
